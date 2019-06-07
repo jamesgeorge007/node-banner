c12132e7f3be8eb098cf5024dbae89f23bbfaffd
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-06-07 09:12:35 +0530

    1.0.2

--- index.js
+++ index.js
@@ -9,13 +9,26 @@ const figlet = require('figlet');
 
 const printTitle = promisify(figlet);
 
+const fallBackCheck = (text, field) => {
+	const pJSON = JSON.parse(
+		fs.readFileSync(path.resolve(process.cwd(), 'package.json')).toString()
+	);
+
+	if (
+		(typeof text === 'undefined' || typeof text !== 'string' || text === '') &&
+		Object.prototype.hasOwnProperty.call(pJSON, field)
+	) {
+		text = pJSON[field];
+	}
+
+	return text;
+};
+
 const showBanner = async (title, tagLine) => {
 	clear();
 
-	const pJSON = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json')).toString());
-	if (typeof tagLine === 'undefined' && Object.prototype.hasOwnProperty.call(pJSON, 'description')) {
-		tagLine = pJSON.description;
-	}
+	title = fallBackCheck(title, 'name');
+	tagLine = fallBackCheck(tagLine, 'description');
 
 	try {
 		const data = await printTitle(title);
--- test.js
+++ test.js
@@ -1,18 +1,37 @@
-const assert = require('assert');
 const showBanner = require('.');
 
-describe('showBanner', () => {
-  it('display the banner', () => {
-    showBanner('Title', 'This is the tagline');
-  });
+describe('Normal usage', () => {
+	it('Displays the banner', async () => {
+		await showBanner('Title', 'This is the tagline');
+	});
 });
 
-describe('no arguments supplied', () => {
-  it('fails without arguments', () => {
-    try {
-      showBanner();
-    } catch (err) {
-      assert.ok(true);
-    }
-  });
+describe('Only title is supplied', () => {
+	it('Fetch tagline content from package.json', async () => {
+		await showBanner('Title');
+	});
+});
+
+describe('Only title is supplied leaving the tagline empty', () => {
+	it('Fetch tagline content from package.json', async () => {
+		await showBanner('Title', '');
+	});
+});
+
+describe('Only tagline is supplied leaving the title empty', () => {
+	it('Fetch title content from package.json', async () => {
+		await showBanner('', 'Tagline');
+	});
+});
+
+describe('No arguments supplied', () => {
+	it('Fetch details from package.json', async () => {
+		await showBanner();
+	});
+});
+
+describe('Argument of type Number is provided', () => {
+	it('Fetch details from package.json', async () => {
+		await showBanner(1);
+	});
 });
