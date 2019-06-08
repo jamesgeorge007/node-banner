16e506b08cc5f4ace081f9d13670fe7ce7588c81
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-06-08 08:16:39 +0530

    1.2.0

--- index.js
+++ index.js
@@ -10,19 +10,24 @@ const printTitle = promisify(figlet);
 const showBanner = async (title, tagLine) => {
 	clear();
 
-	if (typeof title === 'undefined' || typeof tagLine === 'undefined' || title === '' || tagLine === '') {
+	if (
+		typeof title === 'undefined' ||
+		typeof tagLine === 'undefined' ||
+		title === '' ||
+		tagLine === ''
+	) {
 		throw new Error('showBanner() expects to have both the arguments.');
 	}
 
 	if (title === title.toLowerCase()) {
-		const index = title.indexOf('-');
+		const indexOfSeparator = title.indexOf('-');
 
-		if (!index === -1) {
+		if (!indexOfSeparator === -1) {
 			title =
 				title.charAt(0).toUpperCase() +
-				title.substr(1, index - 1) +
+				title.substr(1, indexOfSeparator - 1) +
 				' ' +
-				title.substr(index + 1, title.length).toUpperCase();
+				title.substr(indexOfSeparator + 1, title.length).toUpperCase();
 		} else {
 			title = title.charAt(0).toUpperCase() + title.substr(1, title.length);
 		}
