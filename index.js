b4053261fae9dccf21d38a05a8b38f18acf91def
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-06-30 20:14:41 +0530

    Tweaks
    drop chalk and use kleur

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 'use strict';
 
-const {promisify} = require('util');
-const chalk = require('chalk');
-const clear = require('clear');
+const { promisify } = require('util');
+const kleur = require('kleur');
 const figlet = require('figlet');
 
 const printTitle = promisify(figlet);
 
-const showBanner = async (title, tagLine) => {
-	clear();
+/*
+* All available colors as supported by kleur library.
+*/
+
+const availableColors = ['black', 'red', 'green', 'yellow', 'blue','magenta', 'cyan','white' , 'gray', 'grey'];
+
+/* 
+* Default values for font colors.
+*/
+const defaultOptions = {
+	'titleColor': 'red',
+	'tagLineColor': 'yellow'
+};
+
+
+const showBanner = async (title, tagLine, options) => {
+	/*
+	* Clears the terminal screen.
+	*/
+	process.stdout.write('\u001B[2J\u001B[0;0f');
 
 	if (
 		typeof title === 'undefined' ||
-		typeof tagLine === 'undefined' ||
-		title === '' ||
-		tagLine === ''
+		title === ''
 	) {
-		throw new Error('showBanner() expects to have both the arguments.');
+		throw new Error('The argument title is required.');
 	}
+
+	options = typeof options !== 'undefined' ? options : defaultOptions;
+
+	/*
+	* Convert the title to uppercase if it was provided in lower case.
+	* It's just a convention to have the CLI capitalized.
+	*/
 
 	if (title.toLowerCase().includes('cli') && title === title.toLowerCase()) {
 		const indexOfSeparator = title.indexOf('-');
