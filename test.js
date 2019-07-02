2459d00a8904363d92700527d2f71fe12d905a71
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-07-02 18:47:21 +0530

    Fixup unit tests

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const showBanner = require('.');
 describe('Normal usage', () => {
 	it('Displays the banner', async () => {
 		await showBanner('Title', 'This is the tagline');
+		assert.ok('Works out of the box');
 	});
 });
 
@@ -12,18 +13,15 @@ describe('No arguments are suppleid', () => {
 		try {
 			await showBanner();
 		} catch (err) {
-			assert.equal(err, "Error: showBanner() expects to have both the arguments.");
+			assert.equal(err, "Error: The argument title is required.");
 		}
 	});
 });
 
 describe('Only title is provided', () => {
-	it('should fail', async () => {
-		try {
-			await showBanner('Title');
-		} catch (err) {
-			assert.equal(err, "Error: showBanner() expects to have both the arguments.");
-		}
+	it('should work fine', async () => {
+		await showBanner('Title');
+		assert.ok('tagLine is optional');
 	});
 });
 
@@ -32,7 +30,38 @@ describe('Only tagline is provided', () => {
 		try {
 			await showBanner('', 'This is the tagline');
 		} catch (err) {
-			assert.equal(err, "Error: showBanner() expects to have both the arguments.");
+			assert.equal(err, "Error: The argument title is required.");
 		}
+	});
+});
+
+describe('Customized title color', () => {
+	it('should work absolutely fine', async () => {
+		await showBanner('Title', '', 'magenta');
+		assert.ok('tagLine is optional');
+	});
+});
+
+describe('Ignores empty tagLine', () => {
+	it('avoids the tagline', async () => {
+		await showBanner('Title', ' ', 'blue');
+		assert.ok('whitespaces are ignored');
+	});
+});
+
+describe('Throws an error for out of range titleColor', () => {
+	it('it should fail', async () => {
+		try {
+			await showBanner('Title', '', 'purple');
+		} catch (err) {
+			assert.equal(err, 'RangeError: Title color out of range.');
+		}
+	});
+});
+
+describe('All arguments are provided', () => {
+	it('should work absolutely fine', async () => {
+		await showBanner('Title', 'This is the tagline', 'blue');
+		assert.ok('Everything perfect');
 	});
 });
