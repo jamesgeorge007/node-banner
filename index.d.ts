e2de70168f5fa59883069c633939240b63405099
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-07-02 18:55:54 +0530

    Update type definitions
    index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 export = showBanner;
 
-declare function showBanner(title: string, tagLine: string): Promise<void>;
+declare function showBanner(title: string, tagLine?: string, titleColor?: string): Promise<void>;
 
