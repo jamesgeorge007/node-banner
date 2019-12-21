9e5c7543a2896688fba00ac874a6c2ff8e7da2b3
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-12-21 23:30:09 +0530

    chore: update type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 export = showBanner;
 
-declare function showBanner(title: string, tagLine?: string, titleColor?: string): Promise<void>;
+declare function showBanner(title: string, tagLine?: string, titleColor?: string, tagLineColor?: string): Promise<void>;
 
