02cb2e841291e3d9a517afab5a5aeb804e3179df
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-08-23 11:43:32 +0530

    chore: update ci config

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
-const {promisify} = require('util');
-const kleur = require('kleur');
-const figlet = require('figlet');
+const { promisify } = require("util");
+const kleur = require("kleur");
+const figlet = require("figlet");
 
 const printTitle = promisify(figlet);
 
@@ -11,16 +11,16 @@ const printTitle = promisify(figlet);
  */
 
 const availableColors = [
-	'black',
-	'red',
-	'green',
-	'yellow',
-	'blue',
-	'magenta',
-	'cyan',
-	'white',
-	'gray',
-	'grey'
+  "black",
+  "red",
+  "green",
+  "yellow",
+  "blue",
+  "magenta",
+  "cyan",
+  "white",
+  "gray",
+  "grey"
 ];
 
 /**
@@ -31,21 +31,21 @@ const availableColors = [
  * returns {void}
  */
 const init = (title, titleColor, tagLineColor) => {
-	// Clears the terminal screen.
+  // Clears the terminal screen.
 
-	process.stdout.write('\u001B[2J\u001B[0;0f');
+  process.stdout.write("\u001B[2J\u001B[0;0f");
 
-	if (typeof title === 'undefined' || title === '') {
-		throw new Error('The argument title is required.');
-	}
+  if (typeof title === "undefined" || title === "") {
+    throw new Error("The argument title is required.");
+  }
 
-	if (!availableColors.includes(titleColor)) {
-		throw new RangeError('Title color out of range.');
-	}
+  if (!availableColors.includes(titleColor)) {
+    throw new RangeError("Title color out of range.");
+  }
 
-	if (!availableColors.includes(tagLineColor)) {
-		throw new RangeError('Tagline color out of range.');
-	}
+  if (!availableColors.includes(tagLineColor)) {
+    throw new RangeError("Tagline color out of range.");
+  }
 };
 
 /**
@@ -57,50 +57,50 @@ const init = (title, titleColor, tagLineColor) => {
  * returns {void}
  */
 const showBanner = async (
-	title,
-	tagLine,
-	titleColor = 'red',
-	tagLineColor = 'yellow'
+  title,
+  tagLine,
+  titleColor = "red",
+  tagLineColor = "yellow"
 ) => {
-	// Initialize script.
+  // Initialize script.
 
-	init(title, titleColor, tagLineColor);
+  init(title, titleColor, tagLineColor);
 
-	/**
-	 * Convert the title to uppercase if it was provided in lower case.
-	 * It's just a convention to have the CLI capitalized.
-	 */
+  /**
+   * Convert the title to uppercase if it was provided in lower case.
+   * It's just a convention to have the CLI capitalized.
+   */
 
-	if (title.toLowerCase().includes('cli') && title === title.toLowerCase()) {
-		const indexOfSeparator = title.indexOf('-');
+  if (title.toLowerCase().includes("cli") && title === title.toLowerCase()) {
+    const indexOfSeparator = title.indexOf("-");
 
-		if (indexOfSeparator === -1) {
-			title = title.charAt(0).toUpperCase() + title.substr(1, title.length);
-		} else {
-			title =
-				title.charAt(0).toUpperCase() +
-				title.substr(1, indexOfSeparator - 1) +
-				' ' +
-				title.substr(indexOfSeparator + 1, title.length).toUpperCase();
-		}
-	}
+    if (indexOfSeparator === -1) {
+      title = title.charAt(0).toUpperCase() + title.substr(1, title.length);
+    } else {
+      title =
+        title.charAt(0).toUpperCase() +
+        title.substr(1, indexOfSeparator - 1) +
+        " " +
+        title.substr(indexOfSeparator + 1, title.length).toUpperCase();
+    }
+  }
 
-	try {
-		const data = await printTitle(title);
-		console.log(kleur.bold()[titleColor](data));
+  try {
+    const data = await printTitle(title);
+    console.log(kleur.bold()[titleColor](data));
 
-		// TagLine is optional.
+    // TagLine is optional.
 
-		if (
-			typeof tagLine !== 'undefined' &&
-			tagLine !== '' &&
-			tagLine.trim().length > 0
-		) {
-			console.log(' ' + kleur.bold()[tagLineColor](tagLine));
-		}
-	} catch (error) {
-		throw error;
-	}
+    if (
+      typeof tagLine !== "undefined" &&
+      tagLine !== "" &&
+      tagLine.trim().length > 0
+    ) {
+      console.log(" " + kleur.bold()[tagLineColor](tagLine));
+    }
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = showBanner;
